56b17c7b6f9a9cbe00c7da8ab035446377feafa1
Naman Agarwal <naman4u13@gmail.com>
2018-12-05 02:33:51 +0530

    second commit(added map on current fragment)

--- app/src/main/java/com/example/divyanshsingh/transportationmanagement/fragments/CurrentFragment.java
+++ app/src/main/java/com/example/divyanshsingh/transportationmanagement/fragments/CurrentFragment.java
@@ -1,19 +1,40 @@
 package com.example.divyanshsingh.transportationmanagement.fragments;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.divyanshsingh.transportationmanagement.R;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
 
 
-public class CurrentFragment extends Fragment {
-
-
+public class CurrentFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener {
+    private static final int MY_LOCATION_REQUEST_CODE = 99;
+    ArrayList<LatLng> locs = new ArrayList<LatLng>();
+    GoogleMap mMap;
     public CurrentFragment() {
         // Required empty public constructor
     }
@@ -29,7 +50,115 @@ public class CurrentFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_current, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_current, container, false);
+        SupportMapFragment mapFragment = (SupportMapFragment) this.getChildFragmentManager()
+                .findFragmentById(R.id.current_map);
+
+
+        locs.add(new LatLng(26.457273, 80.349943));
+        locs.add(new LatLng(26.477538, 80.343243));
+        locs.add(new LatLng(26.480797, 80.301492));
+        mapFragment.getMapAsync(this);
+        return view;
+
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+        for (int i = 0; i < locs.size(); i++) {
+            mMap.addMarker(new MarkerOptions().position(locs.get(i)).title("Marker" + i));
+            builder.include(locs.get(i));
+        }
+        LatLngBounds bounds = builder.build();
+        int padding = 50; // offset from edges of the map in pixels
+        final CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, padding);
+        mMap.setOnMapLoadedCallback(new GoogleMap.OnMapLoadedCallback() {
+            @Override
+            public void onMapLoaded() {
+                mMap.animateCamera(cu);
+            }
+        });
+        checkPermission();
+    }
+    void checkPermission() {
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // No explanation needed; request the permission
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_LOCATION_REQUEST_CODE);
+
+        } else {
+            Log.e("TAG", "checkPermission: ");
+            updatemap();
+        }
+    }
+    @SuppressLint("MissingPermission")
+    void updatemap() {
+        mMap.setMyLocationEnabled(true);
+        mMap.setOnMyLocationButtonClickListener(this);
+        // Add a marker in Sydney, Australia, and move the camera.
+
+
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == MY_LOCATION_REQUEST_CODE) {
+            if (permissions.length > 0 &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                Log.e("if", permissions[0]);
+                updatemap();
+            } else {
+                Log.e("else", "else");
+            }
+        }
+
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        isGPSEnable();
+        return false;
+    }
+
+    public void isGPSEnable() {
+
+        LocationManager mlocManager;
+        mlocManager = (LocationManager) getActivity()
+                .getSystemService(Context.LOCATION_SERVICE);
+
+        if (!mlocManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                    getActivity());
+            alertDialogBuilder
+                    .setMessage("GPS is disabled in your device. Enable it?")
+                    .setCancelable(false)
+                    .setPositiveButton("Enable GPS",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog,
+                                                    int id) {
+                                    Intent callGPSSettingIntent = new Intent(
+                                            android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                                    getActivity().startActivity(callGPSSettingIntent);
+                                }
+                            });
+            alertDialogBuilder.setNegativeButton("Cancel",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                        }
+                    });
+            AlertDialog alert = alertDialogBuilder.create();
+            alert.show();
+
+        }
+    }
 }
